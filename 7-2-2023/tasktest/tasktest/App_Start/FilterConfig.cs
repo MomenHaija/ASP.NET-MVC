e7aa4604c2998f9ac0b9828923dc6247f71fe813
momen Abu Alhaija <115459911+MomenHaija@users.noreply.github.com>
2023-02-07 21:18:58 +0300

    Day Task

---
+++ 7-2-2023/tasktest/tasktest/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace tasktest
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
